b1adb9e3ba011580b9d8418d8a66588742cf8d68
Derek Williams <derekwilliams@Dereks-MacBook-Pro.local>
2019-08-26 10:07:51 -0600

    move most styling to css

--- src/components/FieldBoundary.js
+++ src/components/FieldBoundary.js
@@ -18,7 +18,7 @@ class FieldBoundary extends Component {
     this.setState({fieldBoundary: this.state.fieldBoundary})
   }
 
-// for demo purposes only, typically the colors would come from index.css (or elsewhere)
+// for demo purposes only, typically the stroke and fill colors would come from index.css (or elsewhere)
   randomColor() {
     let result = '#';
     for (let i = 0; i < 6; i++) {
@@ -35,25 +35,26 @@ class FieldBoundary extends Component {
   	const height = this.boundaryBoxRef.current != null ?
   	                this.boundaryBoxRef.current.offsetHeight :
   	                1
-
-    const features = [{"type": "Feature", "geometry": this.state.fieldBoundary}]
-  	const projection = geoMercator().fitSize([Math.min(width, height) - 10, Math.min(width, height) - 10], { "type": "FeatureCollection", features })
-    const pathGenerator = geoPath().projection(projection)
-     
-    this.fieldPath = features
-      .map((d, i) => {
-        return <path
-          d={pathGenerator(d)}
-          stroke={this.randomColor()}
-          fill={this.randomColor()}
-          className="field"
-
-      />})
+    if (width !== 1) {
+      const features = [{"type": "Feature", "geometry": this.state.fieldBoundary}]
+      console.log(width)
+    	const projection = geoMercator().fitSize([Math.min(width, height) - 10, Math.min(width, height) - 10], { "type": "FeatureCollection", features })
+      const pathGenerator = geoPath().projection(projection)
+       
+      this.fieldPath = features
+        .map((d, i) => {
+          return <path
+            d={pathGenerator(d)}
+            stroke={this.randomColor()}
+            fill={this.randomColor()}
+            className="field"
+        />})
+    }
   }
   render() {this.drawMap();
     return (
       <div ref={this.boundaryBoxRef} className='field-boundary'>
-          <svg ref={(mapSVG) => this.mapSVG = mapSVG}>
+          <svg viewBox="-5 -5 100 100" ref={(mapSVG) => this.mapSVG = mapSVG}>
               <g>{this.fieldPath}</g>
           </svg>
       </div>
